80d0644db64243996bc9511a49660db733ed6cc2
Rafael Battazza <rafael@multitecsistemas.com.br>
2019-03-22 18:19:30 -0300

    computer play ok

--- tictac.js
+++ tictac.js
@@ -9,6 +9,7 @@ const computerWinnerBase = computerValue * 3;
 
 let endOfGame;
 let winner; // User = 1, Computer = 10 or Draw = 0
+let scoreArray;
 
 let lastPlayerStarted = computerValue;
 
@@ -96,6 +97,7 @@ function winnerVerification(arrayOfResults){
         if(totalPoints == userWinnerBase || totalPoints == computerWinnerBase){
             this.endOfGame = true;
             this.winner = totalPoints == userWinnerBase ? 1 : 10;
+            this.scoreArray[totalPoints == userWinnerBase ? 0 : 1] += 1;
             break;
         }
     }
@@ -103,46 +105,113 @@ function winnerVerification(arrayOfResults){
 
 function computerPlay () {
     if(this.endOfGame)return;
+    let computerPlayed = playWhenSumIs(20); // play to win
 
-    let computerPlayed = false;
-    for (let row = 0; row < 3; row++) {
-        for (let col = 0; col < 3; col++) {
-            if (this.boardMatrix[row][col] == 0) {
-                this.boardMatrix[row][col] = 10;
-                computerPlayed = true;
-                break;
-            }
-        }
-        if(computerPlayed)break;
+    if (!computerPlayed) {
+        computerPlayed = playWhenSumIs(2); // play to dont lost
     }
 
-    // let arrayOfResults = this.sumMatrixData();
-    // let computerPlayed = playToWinComputer(arrayOfResults);
+    if (!computerPlayed) {
+        computerPlayed = playOnCorners();
+    }
 
+    if (!computerPlayed) {
+        playOnEmptyCell();
+    }
 
     endOfGameVerification();
     this.waitingUser = true;
     this.render()
 }
 
-function playToWinComputer(arrayOfResults) {
+function playWhenSumIs(value) {
+    let arrayOfResults = this.sumMatrixData();
     for (let i = 0; i < arrayOfResults.length; i++) {
-        if (arrayOfResults[i] == 20) {
+        if (arrayOfResults[i] == value) {
             this.playOn(i);
-            break;
+            return true;
         }
+    }
+}
+
+function playOnCorners(arrayOfResults, value) {
+    if (this.boardMatrix[0][0] == 0) {
+        this.boardMatrix[0][0] = computerValue;
+        return true;
+    }
+    if (this.boardMatrix[0][2] == 0) {
+        this.boardMatrix[0][2] = computerValue;
+        return true;
+    }
+    if (this.boardMatrix[2][0] == 0) {
+        this.boardMatrix[2][0] = computerValue;
+        return true;
+    }
+    if (this.boardMatrix[2][2] == 0) {
+        this.boardMatrix[2][2] = computerValue;
+        return true;
+    }
+
+    return false;
+}
+
+function playOnEmptyCell () {
+    let computerPlayed = false;
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            if (this.boardMatrix[row][col] == 0) {
+                this.boardMatrix[row][col] = computerValue;
+                computerPlayed = true;
+                break;
+            }
+        }
+        if(computerPlayed)break;
     }
 }
 
 /**
  * @param {*} index 
- * 0,1,2 = linha 0,1,2
- * 3,4,5 = coluna 0,1,2
- * 6 = diagonal desc
- * 7 = diagonal asc
+ * - 0,1,2 = row 0,1,2
+ * - 3,4,5 = column 0,1,2
+ * - 6 = diagonal desc
+ * - 7 = diagonal asc
  */
 function playOn(index) {
+    if(index < 3){
+        let emptyColumn = findEmptyColumnInRow(index);
+        this.boardMatrix[index][emptyColumn] = computerValue;
+    } else if (index < 6) {
+        index = index - 3;
+        let emptyRow = findEmptyRowInColumn(index);
+        this.boardMatrix[emptyRow][index] = computerValue;
+    } else if ( index == 6 || index == 7 ) {
+        let emptyCellArray = findEmptyCellOnDiagonal(index == 6 ? 'desc' : 'asc');
+        this.boardMatrix[emptyCellArray[0]][emptyCellArray[1]] = computerValue;
+    }
+}
 
+function findEmptyColumnInRow(row) {
+    for (let col = 0; col < 3; col++){
+        if (this.boardMatrix[row][col] == 0)return col;
+    }
+}
+
+function findEmptyRowInColumn(column) {
+    for (let row = 0; row < 3; row++){
+        if (this.boardMatrix[row][column] == 0)return row;
+    }
+}
+
+function findEmptyCellOnDiagonal(ascOrDesc) {
+    if(ascOrDesc == 'desc') {
+        if (this.boardMatrix[0][0] == 0) return [0, 0];
+        if (this.boardMatrix[1][1] == 0) return [1, 1];
+        if (this.boardMatrix[2][2] == 0) return [2, 2];
+    } else {
+        if (this.boardMatrix[2][0] == 0) return [2, 0];
+        if (this.boardMatrix[1][1] == 0) return [1, 1];
+        if (this.boardMatrix[0][2] == 0) return [0, 2];
+    }
 }
 
 function render () {
@@ -161,25 +230,33 @@ function render () {
     $("#tableCanvas").html(renderBoard)
 
     $("#msg").html(this.waitingUser ? "Sua vez" : "Aguarde")
+    $("#score").html(`You ${this.scoreArray[0]} | Me ${this.scoreArray[1]}`)
 
     if (this.endOfGame) {
         $("#msg").html(this.winner == 1 ? "Parabéns" : this.winner == 10 ? "Xiii você perdeu" : "Empate");
+        $("#btnNext").show()
     }
 }
 
+function next() {
+    this.reset(true)
+    if (this.lastPlayerStarted == userValue) {
+        this.lastPlayerStarted = computerValue;
+        this.computerPlay()
+    } else {
+        this.lastPlayerStarted = userValue;
+    }
+}
 
-function reset () {
+function reset (softReset) {
     this.waitingUser = true;
     this.endOfGame = false;
     this.boardMatrix = 
                  [[0,0,0],
                   [0,0,0],
                   [0,0,0]];
-
-    if (this.lastPlayerStarted == userValue) {
-        this.lastPlayerStarted = computerValue;
-        this.computerPlay()
-    } else {
+    if (!softReset){
+        this.scoreArray = [0, 0]
         this.lastPlayerStarted = userValue;
     }
 }
